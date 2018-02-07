9fd45170d671c793bbbdc4cf61122e0518d1cc5a
Gennady Verdel <godrose@gmail.com>
2018-02-07 09:27:58 +0200

    #3 Updated versions of the assemblies

--- src/LogoFX.Client.Core.Platform/android/Properties/AssemblyInfo.cs
+++ src/LogoFX.Client.Core.Platform/android/Properties/AssemblyInfo.cs
@@ -1,16 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LogoFX.Client.Core.Platform.Android")]
+[assembly: AssemblyTitle("LogoFX.Client.Core.Platform")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LogoFX.Client.Core.Platform.Android")]
+[assembly: AssemblyProduct("LogoFX.Client.Core.Platform")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +24,5 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.1")]
+[assembly: AssemblyFileVersion("1.2.1")]
